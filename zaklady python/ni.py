5be1b30018643e606bee531a42af1a7c11ceeff4
wormwormik <wormwormik72@seznam.cz>
2025-11-19 14:03:55 +0100

    asddasasdasd

---
+++ zaklady python/ni.py
@@ -0,0 +1,10 @@
+import random
+x = random.randint(1,10)
+inputn = input("uhodni cislo od 1 do 10")
+while x != inputn:
+    print("spatne znovu")
+    inputn = input("uhodni cislo od 1 do 10")
+print("gratuluji")
+
+
+
